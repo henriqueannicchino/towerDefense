eb61eb6da857c185117e562c669cfb4c6906d082
Luis Henrique <luismatossales@hotmail.com>
2022-03-23 12:11:44 -0400

    up

---
+++ enemies/enemy.py
@@ -0,0 +1,64 @@
+import pygame
+
+class Enemy:
+    imgs = []
+    
+    def __init__(self, x, y, widht, height):
+        self.x = x
+        self.y = y
+        self width = width
+        self.height = height
+        self.animation_count = 0
+        self.health = 1
+        self.path = []
+        self.img = None
+
+    def draw(self, win):
+        """
+        Draws the enemy with the given images
+        :param win: surface
+        :return: None
+        """
+        self.animation_count += 1
+        self.img = self.imgs[self.animations_count]
+        if self.animation_count >= len(self.imgs):
+            self.animation_count = 0
+        win.blit(self.img, (self.x, self.y))
+        self.move()
+
+    def collide(self, X, Y):
+        """
+        Returns if position has hit enemy
+        :param x: int
+        :param y: int
+        :return: Bool
+        """
+        if X <= self.x + self.width and X >= self.x:
+            if Y <= self.y + self.height and Y >= self.y:
+                return True
+        return False
+
+    def move(self):
+        """
+        Move enemy
+        :return: None
+        """
+        pass
+
+    def hit(self):
+        """
+        Returns if an enemy has died and removes one health
+        each call
+        :return: Bool
+        """
+        self.health -= 1
+        if self.health <= 0:
+            return True
+
+        return False
+        
+
+
+
+
+        
